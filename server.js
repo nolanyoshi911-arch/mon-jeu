44180a7104723cdb81ce46cdd5285b48f5380680
nolanyoshi911-arch <nolanyoshi911@gmail.com>
2026-08-25 19:22:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,24 +8,47 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-const players = {};
+const players = {}; // id -> { x, y }
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 0, y: 0, name: 'Joueur' };
-    io.emit('players', players);
+    console.log('Nouveau joueur:', socket.id);
 
+    // Initialiser le joueur avec une position aléatoire
+    players[socket.id] = {
+        x: Math.random() * 700,
+        y: Math.random() * 500
+    };
+
+    // Envoyer la liste des joueurs au nouveau
+    socket.emit('players', players);
+
+    // Informer les autres de l'arrivée
+    socket.broadcast.emit('newPlayer', {
+        id: socket.id,
+        ...players[socket.id]
+    });
+
+    // Réception d'un mouvement
     socket.on('move', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
-            socket.broadcast.emit('playerMoved', { id: socket.id, ...players[socket.id] });
+            socket.broadcast.emit('playerMoved', {
+                id: socket.id,
+                x: data.x,
+                y: data.y
+            });
         }
     });
 
+    // Déconnexion
     socket.on('disconnect', () => {
+        console.log('Joueur déconnecté:', socket.id);
         delete players[socket.id];
         io.emit('playerLeft', socket.id);
     });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log('Serveur démarré'));
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Serveur démarré sur le port ' + (process.env.PORT || 3000));
+});
